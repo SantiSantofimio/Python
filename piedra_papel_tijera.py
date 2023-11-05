5f33e35cae49b814f009a2edd40e175577797678
Santiago <san_andres_santo@hotmail.com>
2023-11-05 16:51:40 -0500

    agrego rondas al juego y nmro de victorias o de perdidas, modifico el codigo

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,34 +1,49 @@
 import random
 
-print('Juego de piedra, papel o tijera')
-user = int(input('''
-ingrese: 
-1 para Piedra 
-2 para Papel 
-3 para Tijera
-'''))
+rounds = 1
+wins = 0
+losses = 0
 
-def choice(a):
-  if a == 1:
-      return ('Piedra')
-  elif a == 2:
-      return ('papel')
-  elif a == 3:
-      return ('Tijeras')
-  else:
-      return ('Perder')
+while (wins < 3) and (losses < 3):
+  print("*" * 10)
+  print("Round: ", rounds)
+  print("*" * 10)
+  rounds += 1
 
-def tex():
-  print(f' Has elegido: {choice(user)}')
-  print(f'El enemigo ha elegido: {choice(pc)}')
+  def get_user_choice():
+    print('Juego de piedra, papel o tijera')
+    user_choice = int(
+        input('''
+      ingrese: 
+      1) para Piedra 
+      2) para Papel 
+      3) para Tijera
+      '''))
+    return user_choice
 
-pc = random.randint(1, 3)
-if pc == user:
-    tex()
-    print('Es un empate')
-elif (user == 1 and pc == 3) or (user == 2 and pc == 1) or (user == 3 and pc == 2):
-    tex()
-    print('Has ganado 😎')
-else:
-    tex()
-    print('Has perdido 😭')
+  def choice(a):
+    choices = {1: 'Piedra', 2: 'Papel', 3: 'Tijeras'}
+    return choices.get(a, 'Perder')
+
+  def tex(user, pc):
+    print(f'Has elegido: {choice(user)}')
+    print(f'El enemigo ha elegido: {choice(pc)}')
+
+  def main():
+    global wins, losses
+    user = get_user_choice()
+    pc = random.randint(1, 3)
+
+    tex(user, pc)
+
+    if pc == user:
+      print('Es un empate')
+    elif (user, pc) in [(1, 3), (2, 1), (3, 2)]:
+      print('Has ganado 😎')
+      wins += 1
+    else:
+      print('Has perdido 😭')
+      losses += 1
+
+  if __name__ == "__main__":
+    main()
