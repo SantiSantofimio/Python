06e748d4988db3aae5ea7897b0091d2c56a4a4d3
Santiago <san_andres_santo@hotmail.com>
2023-11-10 21:44:30 -0500

    Le agregué vidas al juego

--- adivida_el_numero.py
+++ adivida_el_numero.py
@@ -2,16 +2,38 @@ import random
 
 
 def run():
+    vidas = 5 
     numero_aleatorio = random.randint(1, 100)
     numer_elegido = int(input('Elige un número del 1 al 100: '))
     
-    while numer_elegido != numero_aleatorio:
-        if numer_elegido < numero_aleatorio:
-            print('Busca un número más grande: ')
+    while (numer_elegido != numero_aleatorio):
+        if (numer_elegido < numero_aleatorio) and (vidas > 1):
+            vidas -= 1
+            print(f'Busca un número más grande, te quedan {vidas} vidas: ')
+            numer_elegido = int(input('Elige otro número: '))
+        elif (numer_elegido > numero_aleatorio) and (vidas > 1):
+            vidas -= 1
+            print(f'Busca un número más pequeño, te quedan {vidas} vidas: ')
+            numer_elegido = int(input('Elige otro número: '))
         else:
-            print('Busca un número más pequeño: ')
-        numer_elegido = int(input('Elige otro número'))
+            print(f'Te quedan {vidas - 1}, parece que has perdido. 😭')
+            return
     print('Ganaste!')
+    
+    # while (numer_elegido != numero_aleatorio):
+    #     if (numer_elegido < numero_aleatorio) and (vidas > 1):
+    #         vidas -= 1
+    #         print(f'Busca un número más grande, te quedan {vidas} vidas: ')
+    #         numer_elegido = int(input('Elige otro número: '))
+    #     elif (numer_elegido > numero_aleatorio) and (vidas > 1):
+    #         vidas -= 1
+    #         print(f'Busca un número más pequeño, te quedan {vidas} vidas: ')
+    #         numer_elegido = int(input('Elige otro número: '))
+    #     elif (numer_elegido != numero_aleatorio) or (vidas == 0):
+    #         print(f'Te quedan {vidas - 1}, parece que has perdido. 😭')
+    #         break
+    #     else:
+    #         print('Ganaste!')
 
 
 if __name__ == '__main__':
